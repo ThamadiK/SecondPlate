dfb6e28492bff5d95d6ff8bb2989b5e3988607d7
Thamadi Kulathunga <s4108774@student.rmit.edu.au>
2026-09-05 23:01:46 +1000

    Update Event and User models

--- src/main/java/com/secondplate/app/model/Event.java
+++ src/main/java/com/secondplate/app/model/Event.java
@@ -6,19 +6,30 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+// This java class represents an entity
 @Entity
+// Stores objects in the "events" table in the database
 @Table(name = "events")
+
+// Lombok automatically generates getters and setters for all fields
 @Getter
 @Setter
 public class Event {
 
+    // The primary key field
     @Id
+    // The database generates valuse for this field automatically. Values auto-increment
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eventId;
 
+    private String title;
     private Long organizerId;   
     private String location;
     private LocalDateTime eventTime;
     private Integer capacity;
     private String description;
+    private String cuisineType; 
+    private String dietaryTags;  
+    private String timeOfDay;   
+    private String imageUrl;     
 }
--- src/main/java/com/secondplate/app/model/User.java
+++ src/main/java/com/secondplate/app/model/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
+    
     private String email;
     private String fullName;
     private String address;
